1e7c7af70a7274be1012a340369be8cf7672de1a
Marcelo Junior <marcelojunior.07@live.com>
2024-09-12 11:48:34 -0300

    update json return

--- routes/sales.js
+++ routes/sales.js
@@ -2,7 +2,52 @@ const express = require('express');
 const { queryDatabase } = require('../db/firebird');
 const router = express.Router();
 
-// Rota para obter vendas detalhadas por período com produtos agrupados
+// Função para calcular o ticket médio
+const calcularTicketMedio = (totalVendas, quantidadeVendas) => {
+    return quantidadeVendas > 0 ? parseFloat(totalVendas / quantidadeVendas).toFixed(2) : 0;
+};
+
+// Função para calcular os itens mais vendidos e o preço médio, agora com a ordenação
+const calcularItensMaisVendidos = (vendas) => {
+    const itensMap = {};
+
+    vendas.forEach(venda => {
+        venda.produtos.forEach(produto => {
+            const idProduto = produto.idProduto;
+
+            if (!itensMap[idProduto]) {
+                itensMap[idProduto] = {
+                    descricao: produto.descricao,
+                    totalVendido: 0,
+                    totalPreco: 0,
+                    quantidadeVendida: 0,
+                };
+            }
+
+            itensMap[idProduto].totalVendido += produto.quantidade;
+            itensMap[idProduto].totalPreco += produto.totalItem;
+            itensMap[idProduto].quantidadeVendida += 1;
+        });
+    });
+
+    // Converte o map para um array e aplica a ordenação
+    const itensMaisVendidos = Object.values(itensMap).map(item => ({
+        descricao: item.descricao,
+        quantidadeVendida: item.totalVendido,
+        totalVendido: parseFloat(item.totalPreco).toFixed(2),
+        precoMedio: parseFloat(item.totalPreco / item.quantidadeVendida).toFixed(2),
+    })).sort((a, b) => {
+        if (b.quantidadeVendida === a.quantidadeVendida) {
+            // Ordena por ordem alfabética quando a quantidade for igual
+            return a.descricao.localeCompare(b.descricao);
+        }
+        // Ordena pela quantidade vendida (maior para menor)
+        return b.quantidadeVendida - a.quantidadeVendida;
+    });
+
+    return itensMaisVendidos;
+};
+
 router.get('/sales', async (req, res) => {
     const { startDate, endDate } = req.query;
 
@@ -12,39 +57,41 @@ router.get('/sales', async (req, res) => {
 
     try {
         const query = `
-      SELECT
-        v.CODIGO,
-        v.DATA_EMISSAO,
-        v.hora,
-        v.nome,
-        u.login,
-        v.SUBTOTAL,
-        v.DESCONTO,
-        v.TOTAL AS TOTAL_VENDA,
-        v.OBSERVACOES,
-        v.SITUACAO,
-        p.item,
-        p.id_produto,
-        prd.descricao,
-        p.qtd,
-        p.total AS TOTAL_ITEM
-      FROM 
-        VENDAS_MASTER v
-      INNER JOIN 
-        VENDAS_DETALHE p ON (v.codigo = p.fkvenda)
-      INNER JOIN 
-        USUARIOS u ON (v.fk_usuario = u.codigo)
-      INNER JOIN
-        PRODUTO prd ON (p.id_produto = prd.codigo)
-      WHERE 
-        v.SITUACAO <> 'C'
-        AND v.DATA_EMISSAO BETWEEN ? AND ?;
-    `;
+            SELECT
+                v.CODIGO,
+                v.DATA_EMISSAO,
+                v.hora,
+                v.nome,
+                u.login,
+                v.SUBTOTAL,
+                v.DESCONTO,
+                v.TOTAL AS TOTAL_VENDA,
+                v.OBSERVACOES,
+                v.SITUACAO,
+                p.item,
+                p.id_produto,
+                prd.descricao,
+                p.qtd,
+                p.total AS TOTAL_ITEM
+            FROM 
+                VENDAS_MASTER v
+            INNER JOIN 
+                VENDAS_DETALHE p ON (v.codigo = p.fkvenda)
+            INNER JOIN 
+                USUARIOS u ON (v.fk_usuario = u.codigo)
+            INNER JOIN
+                PRODUTO prd ON (p.id_produto = prd.codigo)
+            WHERE 
+                v.SITUACAO <> 'C'
+                AND v.DATA_EMISSAO BETWEEN ? AND ?;
+        `;
 
         const result = await queryDatabase(query, [startDate, endDate]);
 
         // Objeto para agrupar vendas e produtos
         const vendasMap = {};
+        let valorTotal = 0; // Variável para somar o valor total das vendas
+        let quantidadeVendas = 0; // Contador de quantidade de vendas
 
         // Processa o resultado agrupando por venda
         result.forEach(row => {
@@ -65,6 +112,10 @@ router.get('/sales', async (req, res) => {
                     situacao: row.SITUACAO,
                     produtos: [] // Inicializa o array de produtos
                 };
+
+                // Incrementa o contador de vendas e o valor total
+                quantidadeVendas++;
+                valorTotal += row.TOTAL_VENDA;
             }
 
             // Adiciona o produto no array "produtos" da venda
@@ -80,7 +131,19 @@ router.get('/sales', async (req, res) => {
         // Converte o objeto para um array de vendas
         const vendas = Object.values(vendasMap);
 
-        res.json(vendas);
+        // Calcula as métricas
+        const ticketMedio = calcularTicketMedio(valorTotal, quantidadeVendas);
+        const itensMaisVendidos = calcularItensMaisVendidos(vendas);
+        valorTotal = parseFloat(valorTotal).toFixed(2);
+
+        // Retorna as vendas com informações adicionais
+        res.json({
+            quantidadeVendas,
+            valorTotal,
+            ticketMedio,
+            itensMaisVendidos,
+            vendas
+        });
     } catch (error) {
         console.error('Erro ao buscar vendas detalhadas:', error);
         res.status(500).json({ error: 'Erro ao buscar dados de vendas.' });
